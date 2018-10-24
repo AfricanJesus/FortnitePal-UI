444bd20ab08ae28ae029d00f6b38eaf6aaa29631
Tyree <tyreegustafson@live.com>
2018-10-24 03:16:11 -0500

    Modify param for getItems to take ID instead of url

--- src/app/shared/services/itemsingle.service.ts
+++ src/app/shared/services/itemsingle.service.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs/Rx";
 
 import Item = ItemSingleModule.Item;
+import "rxjs-compat/add/operator/do";
+import RootObject = ItemSingleModule.RootObject;
 
 
 
@@ -15,9 +17,18 @@ const httpOptions = {
 })
 export class ItemSingleService {
 
+  url: string = 'http://localhost:8080/api/items/';
+
   constructor(private  http: HttpClient) { }
 
-  getItems(url: string): Observable<Item>{
-    return this.http.get<Item>(url);
+  getItem(id: number): Observable<RootObject> {
+    return this.http.get<RootObject>(this.url + id)
+      .do(data => console.log('ITEM: ' + JSON.stringify(data, null, 2)));
+  }
+
+  createItem(item: Item) {
+    let body = JSON.stringify(item, null, 2);
+    console.log(body);
+    return this.http.post('http://localhost:8080/api/items', body, httpOptions);
   }
 }
