252029b7b08363636ecd00bea084d9b7dafbf179
Tyree <tyreegustafson@live.com>
2018-12-13 10:36:27 -0600

    Refactor

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/fortnitepal-ui'));
+app.use(express.static(__dirname + '/dist/angular-starter'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/fortnitepal-ui/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-starter/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
