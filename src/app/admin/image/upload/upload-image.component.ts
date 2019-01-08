abdc85b8250ff7144b3504bd1cc3af998682769b
Tyree <tyreegustafson@live.com>
2019-01-08 04:02:05 -0600

    Add image upload support

--- src/app/admin/image/upload/upload-image.component.ts
+++ src/app/admin/image/upload/upload-image.component.ts
@@ -1,43 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {AppService} from "../../../app.service";
 import {FileService} from "../../../shared/services/file.service";
-import {FileItem, FileUploader} from "ng2-file-upload";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {environment} from "../../../../environments/environment";
 
 
 @Component({
   selector: 'app-admin-upload',
   templateUrl: './upload-image.component.html',
-  styleUrls: ['./upload-image.component.scss']
+  styleUrls: ['./upload-image.component.scss', '../../../../vendor/libs/ngx-sweetalert2/ngx-sweetalert2.scss'],
 })
 
 
 export class UploadImageComponent implements OnInit {
 
-  uploadRequest: UploadRequest;
-  uploader = new FileUploader({});
-
-  constructor(private appService: AppService, private uploadService: FileService) {
-    this.appService.pageTitle = "Upload";
-  }
-
-  ngOnInit() {
-    this.uploadRequest = new UploadRequest('', null);
-  }
-
-  onUpload() {
-    this.changeURLOnAllItems(this.uploader.getNotUploadedItems())
-    this.uploader.uploadAll();
-  }
-
-  changeURLOnAllItems(items: FileItem[]): void {
-    for (let item of items) {
-      item.url = '/upload/' + this.uploadRequest.folderName;
-    }
-  }
-
-  resetForm() {
-    this.uploadRequest = new UploadRequest('', null);
-  }
+  selectedFile: File = null;
+  selectedFileName: String;
+  url = 'http://fortnitepal.s3.amazonaws.com/images/background/basic.png';
+  uploadForm: FormGroup;
+  baseAPIUrl = environment.baseAPIUrl;
 
   folderNames = [
     'backbling',
@@ -56,12 +37,48 @@ export class UploadImageComponent implements OnInit {
     'toy',
     'pet'
   ]
+
+  constructor(private appService: AppService, private uploadService: FileService, private fb: FormBuilder) {
+    this.appService.pageTitle = "Upload";
+  }
+
+  ngOnInit() {
+    this.uploadForm = this.fb.group({
+      folderName: ['', Validators.required],
+      uploadFile: ['', Validators.required]
+    });
+  }
+
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+    this.selectedFileName = this.selectedFile.name;
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event: ProgressEvent) => {
+        this.url = (<FileReader>event.target).result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  resetForm() {
+    this.uploadForm.reset();
+    this.selectedFileName = '';
+    this.url = 'http://fortnitepal.s3.amazonaws.com/images/background/basic.png';
+    console.log(this.baseAPIUrl);
+  }
+
+  onUpload() {
+    this.uploadService.upload(this.uploadForm.get('folderName').value, this.selectedFile).subscribe(
+      () => {
+      }, error1 => {
+      },
+      () => {
+        this.resetForm(), document.getElementById("successModal").click()
+      });
+  }
+
 }
 
-export class UploadRequest {
-  constructor(
-    public folderName: string,
-    public file: File) {
-  }
-}
 
