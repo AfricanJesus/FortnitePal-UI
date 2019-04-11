26b444dd798887a027ba27a2cca8c614b73527bd
tyree-gustafson <tyreegustafson@live.com>
2019-04-11 01:12:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/angular-starter'));
+app.use(express.static(__dirname + '/angular-starter'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-starter/index.html'));
+  res.sendFile(path.join(__dirname + '/angular-starter/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
